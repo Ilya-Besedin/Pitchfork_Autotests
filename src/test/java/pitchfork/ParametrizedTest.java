655f2d9e2b4637271654b973dfa1eda3c75fd908
Ilya Besedin <besedin.work@gmail.com>
2022-03-02 23:54:27 +0300

    added @BeforeEach and @AfterEach

--- src/test/java/pitchfork/ParametrizedTest.java
+++ src/test/java/pitchfork/ParametrizedTest.java
@@ -1,17 +1,32 @@
 package pitchfork;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ParametrizedTest {
+
+    @BeforeEach
+        void precondition() {
+        Configuration.browserSize = "1280x800";
+        open("https://pitchfork.com/");
+    }
+
+    @AfterEach
+        void closeBrowser () {
+        closeWebDriver();
+    }
+
     @Test
     @DisplayName("Checking genre filter in News")
         void setGenreFilterTest() {
-        Selenide.open("https://pitchfork.com/");
         $("li.primary-nav__item").$("a").click();
+
     }
 }
 
