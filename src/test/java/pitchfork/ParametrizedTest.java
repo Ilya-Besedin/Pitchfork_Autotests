55ccb68f96baa4e4cd00e902be906a7c65553af0
Ilya Besedin <besedin.work@gmail.com>
2022-03-03 23:56:07 +0300

    added some selectors

--- src/test/java/pitchfork/ParametrizedTest.java
+++ src/test/java/pitchfork/ParametrizedTest.java
@@ -1,16 +1,25 @@
 package pitchfork;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ParametrizedTest {
+
+    //locators
+    SelenideElement navBar = $("li.primary-nav__item");
+    SelenideElement genreTrigger = $(".genre-menu__trigger");
+    SelenideElement genreMenu = $(".genre-menu__hanging");
+    SelenideElement genreFilterList = $("ul.genre-filters__list");
 
     @BeforeEach
         void precondition() {
@@ -26,10 +35,12 @@ public class ParametrizedTest {
     @Test
     @DisplayName("Checking genre filter in News")
         void setGenreFilterTest() {
-        $("li.primary-nav__item").$("a").click();
-        $(".genre-menu__trigger").click();
-        $(".genre-menu__hanging").$(".genre-menu__clear").click();
-        $("ul.genre-menu__options li").$(byText("Jazz")).click();
+        navBar.$("a").click();
+        genreTrigger.click();
+        genreMenu.$(".genre-menu__clear").click();
+        genreMenu.$(byText("Jazz")).click();
+        genreMenu.$(byText("Update Results")).click();
+        genreFilterList.shouldHave(text("Jazz"));
     }
 }
 
