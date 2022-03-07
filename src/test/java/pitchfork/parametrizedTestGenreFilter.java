e1ace69a3e11aa2dd46388fadc94cbd2965acde5
Ilya Besedin <besedin.work@gmail.com>
2022-03-07 13:31:56 +0300

    added CsvSource tests

--- src/test/java/pitchfork/parametrizedTestGenreFilter.java
+++ src/test/java/pitchfork/parametrizedTestGenreFilter.java
@@ -1,24 +1,29 @@
 package pitchfork;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
-public class ParametrizedTestGenreFilter {
+public class parametrizedTestGenreFilter {
 
     //locators
     SelenideElement navBar = $("li.primary-nav__item");
     SelenideElement genreTrigger = $(".genre-menu__trigger");
     SelenideElement genreMenu = $(".genre-menu__hanging");
     SelenideElement genreFilterList = $("ul.genre-filters__list");
+    ElementsCollection genreFilterNews = $$(".news-collection-fragment");
 
     @BeforeEach
         void precondition() {
@@ -32,7 +37,8 @@ public class ParametrizedTestGenreFilter {
     }
 
     @ValueSource (strings = {"Jazz", "Electronic", "Rap/Hip-Hop", "Metal"})
-    @ParameterizedTest (name = "Checking \"{0}\" filter in News")
+    @ParameterizedTest (name = "Checking set \"{0}\" filter in News")
+    //@Disabled
     //@DisplayName("Checking genre filter in News") - mover to @ParameterizedTest
         void setGenreFilterTest(String genre) {
         navBar.$("a").click();
@@ -42,6 +48,26 @@ public class ParametrizedTestGenreFilter {
         genreMenu.$(byText("Update Results")).click();
         genreFilterList.shouldHave(text(genre));
     }
+
+    @CsvSource (value = {
+            "Jazz| Kamasi Washington Shares New Song “The Garden Path”: Listen",
+            "Electronic| Watch LCD Soundsystem Perform “Thrills” and “Yr City’s a Sucker” on",
+            "Rap/Hip-Hop| Dave Shares Video for New Song “Starlight”: Watch",
+            "Metal| Rage Against the Machine Delay Start of Reunion Tour",
+    },
+            delimiter = '|' //change separator ',' to '|'
+    )
+    @ParameterizedTest (name = "Checking news list by \"{0}\" filter")
+        //@DisplayName("Checking genre filter in News") - mover to @ParameterizedTest
+    void checkGenreFilterContent(String genre, String expectedTest) {
+        navBar.$("a").click();
+        genreTrigger.click();
+        genreMenu.$(".genre-menu__clear").click();
+        genreMenu.$(byText(genre)).click();
+        genreMenu.$(byText("Update Results")).click();
+        genreFilterNews.findBy(text(expectedTest)).shouldBe(visible);
+    }
+
 }
 
 
